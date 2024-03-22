ba80decab9275c848d423ada140f39e18f96cffe
Hubert Ciebiada <hubert.ciebiada@gmail.com>
2024-03-22 13:23:51 +0100

    Initial

---
+++ Program.cs
@@ -0,0 +1,57 @@
+using Audit.Core;
+using Audit.WebApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+Configuration.Setup()
+    .UseElasticsearch(config => config
+        .ConnectionSettings(new Uri("my ES addres"))
+        .Index("my_index")
+        .Id(_ => Guid.NewGuid()));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseAuditMiddleware(x => x
+        .IncludeHeaders()
+        .IncludeRequestBody()
+        .IncludeResponseHeaders()
+        .IncludeResponseBody());
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+}
